e69fbeab7aa88ed7ac9dc26c0da70be4f7ea5dd8
Its-messy <165905254+Its-messy@users.noreply.github.com>
2024-09-11 13:30:25 +0000

    coommiting diseases_prediction_ANN

--- streamlit_DP_app.py
+++ streamlit_DP_app.py
@@ -18,3 +18,26 @@ st.write(f"model {accuracy}")
 
 #User Inputs for real-time predictions
 st.header("Real-Time Prediction")
+#Loading the test data
+test_data = pd.read_csv('Training.csv')
+#Assuming the last column is test
+X_test = test_data.iloc[:, :-1]
+y_test = test_data.iloc[:, -1]
+
+#Assuming the model expects the same input features as X_test
+input_data = []
+for col in X_test.columns:
+    input_value = st.number_input(f"input for {col}", value=0.0)
+    input_data.append(input_value)
+
+#Dataframe for prediction
+input_df = pd.DataFrame([input_data], columns=X_test.columns)
+
+#Make Predictions
+if st.button("Predict"):
+    prediction = model.predict(input_df)
+    st.write(f"Prediction: {prediction[0]}")
+
+#Ploting Summary
+st.header("Accuracy Plot")
+st.bar_chart([float(accuracy.split(':' )[1])])
